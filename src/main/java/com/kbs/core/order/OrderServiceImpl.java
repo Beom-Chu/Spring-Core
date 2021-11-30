ef32ccba38b7b2b2e5454d3fa57e1ddf55aa7a26
kbs <chorocsky@gmail.com>
2021-11-30 19:05:06 +0900

    할인정책 변경

--- src/main/java/com/kbs/core/order/OrderServiceImpl.java
+++ src/main/java/com/kbs/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.kbs.core.order;
 
 import com.kbs.core.discount.DiscountPolicy;
 import com.kbs.core.discount.FixDiscountPolicy;
+import com.kbs.core.discount.RateDiscountPolicy;
 import com.kbs.core.member.Member;
 import com.kbs.core.member.MemberRepository;
 import com.kbs.core.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import com.kbs.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
   private final MemberRepository memberRepository = new MemoryMemberRepository();
-  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+  
+  //할인정책 변경 => 클라이언트 코드에 영향을 줌. OCP 위반.
+//  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
   
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
