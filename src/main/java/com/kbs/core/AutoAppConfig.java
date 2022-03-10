f99270932aeeac24424e327f19426e498ee11f44
kbs <chorocsky@gmail.com>
2022-03-10 15:06:17 +0900

    중복 등록과 충돌

--- src/main/java/com/kbs/core/AutoAppConfig.java
+++ src/main/java/com/kbs/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.kbs.core;
 
+import com.kbs.core.member.MemberRepository;
+import com.kbs.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -7,4 +10,9 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
