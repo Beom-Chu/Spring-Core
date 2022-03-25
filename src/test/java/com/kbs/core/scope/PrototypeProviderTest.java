69a9b67e1d2b3d5c0c8ab94562636734050bf7f3
kbs <chorocsky@gmail.com>
2022-03-25 11:22:13 +0900

    프로토타입, 싱글톤 빈과 함께 사용시 문제점 : 프로토타입 사용할 때마다 스프링 컨테이너에 새로 요청하는 방법

---
+++ src/test/java/com/kbs/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,68 @@
+package com.kbs.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+// 프로토 타입을 사용할 때 마다 스프링 컨테이너에 새로 요청하는 방법
+public class PrototypeProviderTest {
+
+    @Test
+    void providerTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+
+    }
+
+    @Component
+    static class ClientBean {
+
+        @Autowired
+        private ApplicationContext ac;
+
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Component
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
